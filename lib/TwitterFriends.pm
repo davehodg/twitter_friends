cff8ee80f7fc43f731aab08f3230cd8ce243bca5
Dave Hodgkinson <daveh@hodgkinson.org>
2015-10-07 20:39:47 +0100

    auth working

--- lib/TwitterFriends.pm
+++ lib/TwitterFriends.pm
@@ -9,17 +9,17 @@ use Scalar::Util 'blessed';
 
 sub new {
   my $nt = Net::Twitter::Lite::WithAPIv1_1->new(
-                                                consumer_key        => $consumer_key,
-                                                consumer_secret     => $consumer_secret,
-                                                access_token        => $token,
-                                                access_token_secret => $token_secret,
+                                                consumer_key        => $ENV{TWITTER_API_KEY},
+                                                consumer_secret     => $ENV{TWITTER_API_SECRET},
+                                                access_token        => $ENV{TWITTER_ACCESS_TOKEN},
+                                                access_token_secret => $ENV{TWITTER_ACCESS_TOKEN_SECRET},
                                                 ssl                 => 1,
                                                );
 
   #my $result = $nt->update('Hello, world!');
 
   eval {
-    my $statuses = $nt->home_timeline({ since_id => $high_water, count => 100 });
+    my $statuses = $nt->home_timeline({ since_id => 3600, count => 10 });
     for my $status ( @$statuses ) {
       print "$status->{created_at} <$status->{user}{screen_name}> $status->{text}\n";
     }
@@ -27,7 +27,8 @@ sub new {
   if ( my $err = $@ ) {
     die $@ unless blessed $err && $err->isa('Net::Twitter::Lite::Error');
     
-    warn "HTTP Response Code: ", $err->code, "\n",
+    warn
+      "HTTP Response Code: ", $err->code, "\n",
       "HTTP Message......: ", $err->message, "\n",
       "Twitter error.....: ", $err->error, "\n";
   }
